210a4be29ce7f7af6fa1c4b7e2a1f1cdcb74e5ff
Diego-Lara <diegolaraf2002@gmail.com>
2025-11-04 16:11:11 -0600

    Fix: CORS issues

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -3,8 +3,8 @@ import { INestApplication } from "@nestjs/common";
 export class CorsConfig {
     public static EnableCors(app: INestApplication){
         app.enableCors({
-            origin: "*", // add server url
-            methods: ['GET', 'PUT', 'POST', 'DELETE'],
+            origin: '*', // add server url
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
             preflightContinue: true,
             optionsSuccessStatus: 204
         })
