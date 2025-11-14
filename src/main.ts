0836385434c57916b55c5a599e5d65cc68e1f06b
Diego-Lara <diegolaraf2002@gmail.com>
2025-11-13 18:47:25 -0600

    Update

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsConfig } from './config/cors.config';
 import { GlobalPipe } from './common/pipes/global.pipe';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '10mb' }));
   CorsConfig.EnableCors(app);
   GlobalPipe.ValidationPipe(app);
   await app.listen(process.env.PORT ?? 3000);
